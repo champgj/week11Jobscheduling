8067d9456cb427c8ad82243ee2253ec120559395
champgj <champ3415@naver.com>
2021-05-16 22:35:22 +0900

    need to modify

--- src/Jobscheduling.java
+++ src/Jobscheduling.java
@@ -45,103 +45,82 @@ class JobScheduling {
 
     }
 
-    public static int[][] subtaskmaker(int [] task){ // 배열을 주면 그 밑의 부분집합들을 만들어주는 함수
-        int n = task.length;
-        int[][] subtask = new int [(int) Math.pow(2,n)][n];
-        int k = 0,p=0;
-        for (int i = 0; i < (1<<n); i++)
-        {
-            for (int j = 0; j < n; j++) {
-                if ((i & (1 << j)) > 0) {
-                    subtask[k][p] = task[j];
-                    p++;
-                }
-            }
-            p=0;
-            k++;
-        }
-        /*
-        System.out.println("모든 부분집합은");
-        for(int i = 0; i<subtask.length;i++){
-            for(int j=0;j<subtask[i].length;j++){
-                if(subtask[i][j] != 0)
-                    System.out.print(subtask[i][j]+" ");
-            }
-            System.out.println("");
-        }
-
- */
-        return subtask;
-    }
-
-    public static int bruteforce(int []machine, int [] task){ // 브루트포스 그냥 기계 대수에 나눠서 작업 분배
+    public static int bruteforce(int[] task, int [] machine){ // 브루트포스 그냥 기계 대수에 나눠서 작업 분배
         int m = machine.length;
         int n = task.length;
+        int[] taskmachineNum = new int[n];                //각 작업이 실행되는 기계가 몇번 기계인지 저장하는 배열
+        int[] lastarr = new int[(int) Math.pow(m, n)];    //각 경우에서의 마지막 종료 시간을 저장하는 배열
 
-        int[][] subtask = subtaskmaker(task); // task를 부분집합으로 나눈다.
 
 
-        int [] subtasklength = new int[(int) Math.pow(2,n)]; // 각각 부분집합의 합을 구해본다. 그리고 그것을  subtasklength 배열에 저장
-        int sum = 0;
-        for(int i =0; i<(int) Math.pow(2,n);i++){
-            for(int j = 0; j<n;j++){
-                sum += subtask[i][j];
-            }
-            subtasklength[i] = sum;
-            sum=0;
-        }
+        for (int i = 0; i < (int) Math.pow(m, n); i++) {
+            zeromaker(machine); // 한번 루프가 돌았다면 machine에 이전 작업결과들이 남아있을테니 다시 0으로 모두 초기화해준다.
 
-        int min = 10000; //최소값에 일반적인 값보다 훨씬 큰 값을 넣어 두고
-        int memoryi=0;
-        for(int i = 0; i<subtasklength.length/2;i++){
-            if(min>Math.abs(subtasklength[i] - subtasklength[subtasklength.length-i-1])) {
-                min = Math.abs(subtasklength[i] - subtasklength[subtasklength.length - i-1]);
-                memoryi = i;
+            for (int k = 0; k < n; k++) { //작업을 기계와 매치시키는 과정
+                int p = taskmachineNum[k];
+                machine[p] += task[k];  // task길이를 기계의 종료시간에 추가연장해준다.
             }
 
+            taskmachineNum[0]++;
+            for (int o = 0; o < n; o++) {    //작업이 들어갈 기계번호를 바꿔주는 과정
+                if (m-1 < taskmachineNum[o]) {
+                    taskmachineNum[o] = 0;
+                    if (o + 1 < n) taskmachineNum[o + 1]++;
+
+                }
+            }
+
+
+            for (int l = 0; l < m; l++) {// 각 경우에서 가장 느렸던 종료시간을 lastarr배열에 저장하는 과정
+                if (machine[l] > lastarr[i]) { //만약 더 크면->더 느리면
+                    lastarr[i] = machine[l];  // 그 값을 last arr에 저장
+                }
+            }
+
         }
 
-        System.out.println(memoryi+"번째의 subtasklength가 차이가 제일 작음");
-        System.out.println("i번째 subtasklength는 일처리를 기계1이 ");
-        for(int j=0;j<n;j++){
-            if(subtask[subtask.length-memoryi-1][j] != 0)
-                System.out.print(subtask[memoryi][j]+" ");
+
+
+        int min = 10000;
+        for (int i = 0; i < (int) Math.pow(m, n); i++) {           //모든 경우에서 가장 작은 값 반환
+            if (lastarr[i] < min) {
+                min = lastarr[i];
+            }
         }
-        System.out.println("만큼 하고 나머지 기계2가 ");
-        for(int j=0;j<n;j++){
-            if(subtask[subtask.length-memoryi-1][j] != 0)
-                System.out.print(subtask[subtask.length-memoryi-1][j]+" ");
-        }
-
-        System.out.println("그래서 최종적으로 bruteforce 기계2대로 했을 때의 걸리는 시간은 ");
-        int max = Math.max(subtasklength[memoryi],subtasklength[subtask.length - memoryi-1] );
-
-        System.out.println(max + "초 이다");
+        return min;
 
 
-        return max;
     }
 
+
+    public static int[] zeromaker(int [] machine) {
+        for (int j = 0; j < machine.length; j++) {
+            machine[j] = 0; // 매 번 돌릴 때마다 기계의 마지막 종료시간을 다시 0으로 초기화 해준다.
+        }
+        return machine;
+    }
 
 
     public static void main(String[] arg){
 
-        System.out.print("입력의 개수를 입력하세요>");
+        System.out.print("입력의 개수와 기계대수를 입력하세요>");
         Scanner scanner = new Scanner(System.in);
         int totaltask = scanner.nextInt();
+        int totalmachine = scanner.nextInt();
 
         int task[] = new int[totaltask];
         randomtask(task); //이렇게 하면 task배열의 값들이 모두 랜덤으로 설정된다.
 
-        int machine[] = new int[2]; // 문제에서 기계댓수는 2개
-        // 그리고 각각의 machineNum 배열에는 기계가 현재 하고 있는일이 끝나는시간이 들어가 있다.
-        // ppt에서는 L로 표현을 하신 것 같다.
+        int machine[] = new int[totalmachine]; // 문제에서 기계댓수는 2개
+        // 그리고 각각의 machine 배열에는 기계가 현재 하고 있는일이 끝나는시간이 들어가 있다.
+        // ppt에서는 L로 표현을 하신 것 같다. 같은 것!
 
         // 랜덤이므로 생성된 배열의 값들을 한번 확인한다.
-        System.out.println("랜덤생성된 작업의 길이들은");
+        System.out.println("랜덤생성된 작업들은");
         for (int i=0;i<task.length;i++){
             System.out.print(task[i]+" ");
         }
+        System.out.println("");
         System.out.println("");
         System.out.println("greedy로 구현시 최종 걸리는 시간은 "+JobScheduling(machine,task)+"초 입니다.");
 
@@ -152,6 +131,6 @@ class JobScheduling {
 
 
         System.out.println("");
-        System.out.println("bruteforce로 구현시 최종 걸리는 시간은 "+bruteforce(machine,task)+"초 입니다.");
+        System.out.println("bruteforce로 구현시 최종 걸리는 시간은 "+bruteforce(task,machine)+"초 입니다.");
     }
 }
