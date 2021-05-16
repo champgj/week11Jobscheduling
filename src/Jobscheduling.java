a5c3bf75ea1f0a73e509bd2464ebf2657cab8010
champgj <champ3415@naver.com>
2021-05-16 14:50:02 +0900

    need to modify/ bruteforce two machines

--- src/Jobscheduling.java
+++ src/Jobscheduling.java
@@ -46,23 +46,76 @@ class JobScheduling {
     }
 
 
-    public static int bruteforce(int []machine, int [] task){ // 브루트포스 그냥 기계 대수에 나눠서 작업 분배
+    public static void bruteforce(int []machine, int [] task){ // 브루트포스 그냥 기계 대수에 나눠서 작업 분배
         int m = machine.length;
         int n = task.length;
 
-        for(int i = 0; i<n;i++){
-            machine[i%m] += task[i];
+        int[][] subtask = new int [(int) Math.pow(2,n)][n];
+        int k = 0,p=0;
+        for (int i = 0; i < (1<<n); i++)
+        {
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) > 0) {
+                    subtask[k][p] = task[j];
+                    p++;
+                }
+            }
+            p=0;
+            k++;
+        }
+        System.out.println("모든 부분집합은");
+        for(int i = 0; i<subtask.length;i++){
+            for(int j=0;j<subtask[i].length;j++){
+                if(subtask[i][j] != 0)
+                    System.out.print(subtask[i][j]+" ");
+            }
+            System.out.println("");
         }
 
-        int max = machine[0];
-        for(int i =0; i<m;i++){
-            if(max < machine[i]){
-                max = machine[i];
+        int [] subtasklength = new int[(int) Math.pow(2,n)];
+
+        int sum = 0;
+
+        for(int i =0; i<(int) Math.pow(2,n);i++){
+            for(int j = 0; j<n;j++){
+                sum += subtask[i][j];
+            }
+            subtasklength[i] = sum;
+            sum=0;
+
+        }
+
+        for(int i =0; i< subtasklength.length;i++){
+            System.out.println(subtasklength[i]);
+        }
+        int min = 100;
+        int memoryi=0;
+        for(int i = 0; i<subtasklength.length/2;i++){
+            if(min>Math.abs(subtasklength[i] - subtasklength[subtasklength.length-i-1])) {
+                min = Math.abs(subtasklength[i] - subtasklength[subtasklength.length - i-1]);
+                memoryi = i;
             }
 
         }
+        System.out.println(memoryi+"번째의 subtasklength가 차이가 제일 작음");
+        System.out.println("i번째 subtasklength는 일처리를 기계1이 ");
+            for(int j=0;j<n;j++){
+                if(subtask[memoryi][j] != 0)
+                    System.out.print(subtask[memoryi][j]+" ");
+            }
+        System.out.println("만큼 하고 나머지 기계2가 ");
+        for(int j=0;j<n;j++){
+            if(subtask[subtask.length-memoryi-1][j] != 0)
+                System.out.print(subtask[subtask.length-memoryi-1][j]+" ");
+        }
 
-        return max; // 제일 오래걸리는 기계의 종료시간을 반환
+        System.out.println("그래서 최종적으로 bruteforce 기계2대로 했을 때의 걸리는 시간은 ");
+        int max = Math.max(subtasklength[memoryi],subtasklength[subtask.length - memoryi-1] );
+
+        System.out.println(max + "초 이다");
+
+
+
     }
 
 
@@ -74,7 +127,7 @@ class JobScheduling {
         int totaltask = scanner.nextInt();
         int totalmachine = scanner.nextInt();
 
-        int task[] = new int[(int) Math.pow(2,totaltask)];
+        int task[] = new int[totaltask];
         randomtask(task); //이렇게 하면 task배열의 값들이 모두 랜덤으로 설정된다.
 
         int machine[] = new int[totalmachine]; // 입력받지만, 문제에서 기계댓수는 2개
@@ -89,13 +142,14 @@ class JobScheduling {
         System.out.println("");
         System.out.println("greedy로 구현시 최종 걸리는 시간은 "+JobScheduling(machine,task)+"초 입니다.");
 
-        for(int i = 0; i<machine.length;i++){ // machine 배열(종료시간 저장배열)에
-                                                // 기존 greedy로 구현해서 저장된 값들을 다시 0으로 초기화해준다.
-            machine[i] = 0;
-        }
+
+
+       for(int i = 0; i<machine.length;i++){
+           machine[i]=0;
+       }
 
         System.out.println("");
-        System.out.println("bruteforce로 구현시 최종 걸리는 시간은 "+bruteforce(machine,task)+"초 입니다.");
+       bruteforce(machine,task);
 
 
 
